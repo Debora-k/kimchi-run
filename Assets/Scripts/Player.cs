6b917b651d27709321b767ce84d9ed7aa8c642f9
Debora-k <debora0104k@gmail.com>
2025-01-11 22:11:20 -0700

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [Header("Settings")]
+    public float JumpForce;
+
+    [Header("References")]
+    public Rigidbody2D PlayerRigidBody;
+
+    public Animator PlayerAnimator;
+
+    //if the player is on the ground 
+    private bool isGrouneded = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //when a user press 'space'
+        if (Input.GetKeyDown(KeyCode.Space) && isGrouneded)
+        {
+            PlayerRigidBody.AddForceY(JumpForce, ForceMode2D.Impulse);
+            isGrouneded = false;
+            PlayerAnimator.SetInteger("state", 1);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Platform")
+        {
+            //when this game begin
+            if (!isGrouneded)
+            {
+                PlayerAnimator.SetInteger("state", 2);
+            }
+            isGrouneded = true;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject[] gameObjects;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
