9cd24a50681464d25cff69cebc53bd25b0642be7
Debora-k <debora0104k@gmail.com>
2025-01-13 23:40:01 -0700

    working on this game

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,10 +8,17 @@ public class NewMonoBehaviourScript : MonoBehaviour
     [Header("References")]
     public Rigidbody2D PlayerRigidBody;
 
+    public BoxCollider2D PlayerCollider;
+
     public Animator PlayerAnimator;
 
     //if the player is on the ground 
     private bool isGrouneded = true;
+
+    //given lives is 3 to a player
+    public int lives = 3;
+    public bool isInvincible = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -30,6 +37,38 @@ public class NewMonoBehaviourScript : MonoBehaviour
         }
     }
 
+    void KillPlayer()
+    {
+        PlayerCollider.enabled = false;
+        PlayerAnimator.enabled = false;
+        PlayerRigidBody.AddForceY(JumpForce, ForceMode2D.Impulse);
+    }
+    void Hit()
+    {
+        lives -= 1;
+        if (lives == 0)
+        {
+            KillPlayer();
+        }
+
+    }
+
+    void Heal()
+    {
+        lives = Mathf.Min(3, lives + 1);
+    }
+
+    void StartInvincible()
+    {
+        isInvincible = true;
+        //after 5s then make it stop!
+        Invoke("StopInvincible", 5f);
+    }
+
+    void StopInvincible()
+    {
+        isInvincible = false;
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Platform")
@@ -48,15 +87,22 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         if (collider.gameObject.tag == "enemy")
         {
+            if (!isInvincible)
+            {
+                Destroy(collider.gameObject);
+                Hit();
+            }
 
         }
         else if (collider.gameObject.tag == "food")
         {
-
+            Destroy(collider.gameObject);
+            Heal();
         }
         else if (collider.gameObject.tag == "golden")
         {
-
+            Destroy(collider.gameObject);
+            StartInvincible();
         }
     }
 }
